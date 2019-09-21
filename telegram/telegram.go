aad9cbdbad464860bbfb7c46a59c627a26e4c78e
SerhiiCho <serhiicho@protonmail.com>
2019-09-21 21:18:06 +0300

    Changed comment in telegram.go

--- telegram/telegram.go
+++ telegram/telegram.go
@@ -1,22 +1,21 @@
 package telegram
 
 import (
+	"github.com/SerhiiCho/shoshka_go/models"
 	"os"
 
 	"github.com/SerhiiCho/shoshka_go/utils"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-/*
-SendMessage sends message in telegram chat
-*/
+// SendMessage sends message in telegram chat
 func SendMessage(message string) {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
 	utils.HandleError(err, "Bot init error")
 
 	msg := tgbotapi.NewMessage(getChatID(), message)
 
-	bot.Send(msg)
+	_, _ = bot.Send(msg)
 }
 
 func SendMessagesWithNewPhotoReports(newReports []models.PhotoReport) {
