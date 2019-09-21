e4642709c28178f4af79b48828926729567a8fde
SerhiiCho <serhiicho@protonmail.com>
2019-09-21 13:16:11 +0300

    Added contains and getUniqueItem functions

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 
@@ -13,10 +14,31 @@ func init() {
 	system.LoadEnvPackage()
 }
 
-func saveTitlesToFile(titles []string) {
+func saveTitlesIntoFile(titles []string) {
 	file, _ := json.MarshalIndent(titles, "", " ")
-	err := ioutil.WriteFile("hello.txt", file, 0644)
+	err := ioutil.WriteFile("hello", file, 0644)
 	utils.HandleError(err, "Can't write to a file")
+}
+
+func contains(slice []string, needle string) bool {
+	for _, sliceItem := range slice {
+		if sliceItem == needle {
+			return true
+		}
+	}
+	return false
+}
+
+func getUniqueItem(slice1 []string, slice2 []string) []string {
+	result := []string{}
+
+	for _, item1 := range slice1 {
+		if !contains(slice2, item1) {
+			result = append(result, item1)
+		}
+	}
+
+	return result
 }
 
 func main() {
@@ -26,7 +48,7 @@ func main() {
 	linksHTML := system.GetLinksFromHTML(html)
 	photoReports := system.GetAllInformation(linksHTML)
 
-	var titles []string
+	titles := []string{}
 
 	for _, photoReport := range photoReports {
 		titles = append(titles, photoReport.Title)
@@ -34,5 +56,16 @@ func main() {
 		// log.Println(photoReport.Title)
 	}
 
-	saveTitlesToFile(titles)
+	oldContext := utils.FileGetContents("hello")
+	oldSlice := []string{}
+	json.Unmarshal([]byte(oldContext), &oldSlice)
+	// saveTitlesIntoFile(titles)
+	diff := getUniqueItem(titles, oldSlice)
+
+	if len(diff) == 0 {
+		println("No diff")
+		return
+	}
+
+	fmt.Printf("%#v\n", diff)
 }
